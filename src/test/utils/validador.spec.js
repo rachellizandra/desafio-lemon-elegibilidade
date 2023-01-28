b55e874f4ad5fd091a50e10215ec912153b4cbee
Rachel <rachellizandra@outlook.com>
2023-01-28 12:46:59 -0300

    adiciona teste para validação do cpf e cnpj

--- src/test/utils/validador.spec.js
+++ src/test/utils/validador.spec.js
@@ -12,9 +12,21 @@ const {
 // A função tostrictEqual valida atributos, ex: json, arrays, objetos
 
 describe("Validador", () => {
-    it("Deveria lançar um Type error com um número de documento inválido", () => {
-        const numeroDoDocumento = "362569";
+  it("Deveria lançar um Type error com um número de documento inválido", () => {
+    const numeroDoDocumento = "362569";
 
-        expect(() => validarNumeroDocumento(numeroDoDocumento)).toThrow();
-    });
+    expect(() => validarNumeroDocumento(numeroDoDocumento)).toThrow();
+  });
+
+  it("Deve verificar se o CPF for válido", () => {
+    const cpf = "14254778596";
+
+    expect(validarNumeroDocumento(cpf)).toBeUndefined();
+  });
+
+  it("Deve verificar se o CNPJ for válido", () => {
+    const cnpj = "12546398754265";
+
+    expect(validarNumeroDocumento(cnpj)).toBeUndefined();
+  });
 });
