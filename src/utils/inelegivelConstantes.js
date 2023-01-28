0812cf8c645720a9c243fa1347bc81804c85b10b
Rachel <rachellizandra@outlook.com>
2023-01-28 12:06:24 -0300

    adiciona as mensagens que serão retornadas caso algum critério de inegibilidade seja válido

---
+++ src/utils/inelegivelConstantes.js
@@ -0,0 +1,9 @@
+"use strict";
+
+exports.CLASSE_DE_CONSUMO = "Classe de consumo não aceita";
+
+exports.MODALIDADE_TARIFARIA = "Modalidade tarifária não aceita";
+
+exports.CONEXAO_SUPORTADA = "Tipo de conexão não suportado";
+
+exports.CONSUMO_BAIXO = "Consumo muito baixo para tipo de conexão";
