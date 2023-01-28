5b4d78c5f75a61df47bfd9885bf4089825cb0e7a
Rachel <rachellizandra@outlook.com>
2023-01-28 11:33:05 -0300

    adiciona o enum de classe de consumo

---
+++ src/enums/classesDeConsumoEnum.js
@@ -0,0 +1,15 @@
+"use strict";
+
+exports.classesDeConsumoEnum = [
+  "residencial",
+  "industrial",
+  "comercial",
+  "rural",
+  "poderPublico",
+];
+
+exports.classesDeConsumoElegiveisEnum = [
+  "residencial",
+  "industrial",
+  "comercial",
+];
