4f652021da84587b98b280a75404cd4a3f09216e
Rachel <rachellizandra@outlook.com>
2023-01-28 11:54:10 -0300

    adiciona teste para verificar se o cliente é elegível ou inelegível

---
+++ src/test/avaliarCliente.spec.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const { avaliarCliente } = require("../avaliarCliente");
+
+//Comando para rodar o teste -> npm run test:coverage
+
+describe("Avaliar cliente", () => {
+  it("Deveria retornar um cliente elegível", () => {
+    // cenário
+    const input = {
+      numeroDoDocumento: "14041737706",
+      tipoDeConexao: "bifasico",
+      classeDeConsumo: "comercial",
+      modalidadeTarifaria: "convencional",
+      historicoDeConsumo: [
+        3878, 9760, 5976, 2797, 2481, 5731, 7538, 4392, 7859, 4160, 6941, 4597,
+      ],
+    };
+
+    const expectedOutput = {
+      //cenário
+      elegivel: true,
+      economiaAnualDeCO2: 5553.24,
+    };
+
+    const output = avaliarCliente(input); //comportamento
+
+    expect(output).toStrictEqual(expectedOutput); //expectativa
+  });
+
+  it("Deveria retornar um cliente não elegível", () => {
+    // cenário
+    const input = {
+      numeroDoDocumento: "14041737706",
+      tipoDeConexao: "bifasico",
+      classeDeConsumo: "rural",
+      modalidadeTarifaria: "verde",
+      historicoDeConsumo: [
+        3878, 9760, 5976, 2797, 2481, 5731, 7538, 4392, 7859, 4160,
+      ],
+    };
+
+    const expectedOutput = {
+      elegivel: false,
+      razoesInelegibilidade: [
+        "Classe de consumo não aceita",
+        "Modalidade tarifária não aceita",
+      ],
+    };
+
+    const output = avaliarCliente(input); //comportamento
+
+    expect(output).toStrictEqual(expectedOutput); //expectativa
+  });
+});
