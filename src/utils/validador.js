b00fa9144dbc5d59a785da4a4820630600b23d95
Rachel <rachellizandra@outlook.com>
2023-01-28 11:36:36 -0300

    adiciona arquivo validador.js para armazenar as funções de validação

--- src/utils/validador.js
+++ src/utils/validador.js
@@ -1,1 +1,21 @@
-s
+"use strict";
+
+exports.validarNumeroDocumento = (numeroDocumento) => {
+    return numeroDocumento;
+}
+
+exports.validarTipoDeConexao = (tipoDeConexao) => {
+    return tipoDeConexao;
+}
+exports.validarClasseConsumo = (classeDeConsumo) => {
+    return classeDeConsumo;
+}
+
+exports.validarModalidadeTarifaria = (modalidadeTarifaria) => {
+    return modalidadeTarifaria;
+}
+
+exports.validarHistoricoConsumo = (mediaDeconsumo, tipoDeConexao) => {
+    return mediaDeconsumo;
+}
+
